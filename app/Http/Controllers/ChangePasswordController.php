8758ae2b685e90f6c40dec6a893cb0553cb62ae2
venix12 <chopek.micha@gmail.com>
2019-09-30 18:45:47 +0200

    add missing space

--- app/Http/Controllers/ChangePasswordController.php
+++ app/Http/Controllers/ChangePasswordController.php
@@ -40,7 +40,7 @@ class ChangePasswordController extends Controller
         }
     }
 
-   public function __construct()
+    public function __construct()
     {
         $this->middleware('auth');
     }

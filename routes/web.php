55283f49c23385d1793785dd2a8624e106b1475d
venix12 <chopek.micha@gmail.com>
2019-10-04 20:49:20 +0200

    get osu oauth token

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -62,4 +64,42 @@ Route::group(['prefix' => 'spotlights'], function() {
     Route::post('/remove-nomination', 'SpotlightsNominationController@destroy')->name('spotlights.removeNomination');
     Route::post('/vote', 'SpotlightsNominationVoteController@index')->name('spotlights.vote');
     Route::post('/update-vote', 'SpotlightsNominationVoteController@update')->name('spotlights.updateVote');
+});
+
+//oauth
+Route::get('/oauth', function (Request $request) {
+    $request->session()->put('state', $state = Str::random(40));
+
+    $query = http_build_query([
+        'client_id' => env('CLIENT_ID'),
+        'redirect_uri' => 'http://spotlights.team/callback',
+        'response_type' => 'code',
+        'scope' => '',
+        'state' => $state,
+    ]);
+
+    return redirect('http://osu.ppy.sh/oauth/authorize?'.$query);
+})->name('oauth');
+
+Route::get('/callback', function (Request $request) {
+    $state = $request->session()->pull('state');
+
+    throw_unless(
+        strlen($state) > 0 && $state === $request->state,
+        InvalidArgumentException::class
+    );
+
+    $http = new GuzzleHttp\Client;
+
+    $response = $http->post('http://osu.ppy.sh/oauth/token', [  
+        'form_params' => [
+            'grant_type' => 'authorization_code',
+            'client_id' => env('CLIENT_ID'),
+            'client_secret' => env('CLIENT_SECRET'),
+            'redirect_uri' => 'http://spotlights.team/callback',
+            'code' => $request->code,
+        ],
+    ]);
+
+    return json_decode((string) $response->getBody(), true);
 });

c28844c74de0b23b620912eb2b8e1a5f2788b50d
venix12 <chopek.micha@gmail.com>
2019-10-14 12:59:32 +0200

    put commas on voters list properly

--- resources/views/spotlights/show/expandable/voters.blade.php
+++ resources/views/spotlights/show/expandable/voters.blade.php
@@ -1,15 +1,11 @@
 <div class="form-row">
     <div class="text-success">Supporters ({{count($votes->where('nomination_id', $nomination->id)->where('value', 1))}}):&nbsp;</div>
 
-    @foreach($votes as $vote)
-        @if($vote->value === 1)
-            @if($vote->nomination_id === $nomination->id)
-                @if($users->find($vote->user_id)->active)
-                    <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a>, &nbsp;
-                @else
-                    <div class="text-gray">{{$users->find($nomination->user_id)->username}},</div>
-                @endif
-            @endif
+    @foreach($votes->where('nomination_id', $nomination->id)->where('value', '===', 1) as $vote)
+        @if($users->find($vote->user_id)->active)
+            <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a> {!!$loop->last ? '' : ',&nbsp;'!!}
+        @else
+            <div class="text-gray">{{$users->find($nomination->user_id)->username}} {!!$loop->last ? '' : ',&nbsp;'!!}</div>
         @endif
     @endforeach
 </div>
@@ -17,15 +13,11 @@
 <div class="form-row">
     <div class="text-danger">Criticizers ({{count($votes->where('nomination_id', $nomination->id)->where('value', -1))}}):&nbsp;</div>
 
-    @foreach($votes as $vote)
-        @if($vote->value === -1)
-            @if($vote->nomination_id === $nomination->id)
-                @if($users->find($vote->user_id)->active)
-                    <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a>, &nbsp;
-                @else
-                    <div class="text-gray">{{$users->find($nomination->user_id)->username}},</div>
-                @endif
-            @endif
+    @foreach($votes->where('nomination_id', $nomination->id)->where('value', '===', -1) as $vote)
+        @if($users->find($vote->user_id)->active)
+            <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a> {!!$loop->last ? '' : ',&nbsp;'!!}
+        @else
+            <div class="text-gray">{{$users->find($nomination->user_id)->username}} {!!$loop->last ? '' : ',&nbsp;'!!}</div>
         @endif
     @endforeach
 </div>
@@ -33,15 +25,11 @@
 <div class="form-row">
     Neutral ({{count($votes->where('nomination_id', $nomination->id)->where('value','===', 0))}}):&nbsp;
 
-    @foreach($votes as $vote)
-        @if($vote->value === 0)
-            @if($vote->nomination_id === $nomination->id)
-                @if($users->find($vote->user_id)->active)
-                    <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a>, &nbsp;
-                @else
-                    <div class="text-gray">{{$users->find($nomination->user_id)->username}},</div>
-                @endif
-            @endif
+    @foreach($votes->where('nomination_id', $nomination->id)->where('value', '===', 0) as $vote)
+        @if($users->find($vote->user_id)->active)
+            <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a> {!!$loop->last ? '' : ',&nbsp;'!!}
+        @else
+            <div class="text-gray">{{$users->find($nomination->user_id)->username}} {!!$loop->last ? '' : ',&nbsp;'!!}</div>
         @endif
     @endforeach
 </div>
@@ -49,15 +37,11 @@
 <div class="form-row">
     Contributors ({{count($votes->where('nomination_id', $nomination->id)->where('value', 2))}}):&nbsp;
 
-    @foreach($votes as $vote)
-        @if($vote->value === 2)
-            @if($vote->nomination_id === $nomination->id)
-                @if($users->find($vote->user_id)->active)
-                    <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a>, &nbsp;
-                @else
-                    <div class="text-gray">{{$users->find($nomination->user_id)->username}},</div>
-                @endif
-            @endif
+    @foreach($votes->where('nomination_id', $nomination->id)->where('value', '===', 2) as $vote)
+        @if($users->find($vote->user_id)->active)
+            <a href={{route('user.profile', ['id' => $vote->user_id])}}>{{$users->find($vote->user_id)->username}}</a> {!!$loop->last ? '' : ',&nbsp;'!!}
+        @else
+            <div class="text-gray">{{$users->find($nomination->user_id)->username}} {!!$loop->last ? '' : ',&nbsp;'!!}</div>
         @endif
     @endforeach
 </div>

c5975acf295e9331fbeb586040661b2618de674d
venix12 <chopek.micha@gmail.com>
2019-12-23 18:20:52 +0100

    remove unused assets from welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,6 @@
         <!-- Styles -->
         <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="{{ asset('css/font-awesome.css') }}" rel="stylesheet">
 
         <style>
             html, body {

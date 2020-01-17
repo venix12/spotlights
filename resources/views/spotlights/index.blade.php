6933950dfb53f191b68bb400721da05c1551dff7
venix12 <chopek.micha@gmail.com>
2020-01-17 18:35:21 +0100

    add description to spotlights-results

--- resources/views/spotlights/index.blade.php
+++ resources/views/spotlights/index.blade.php
@@ -4,26 +4,36 @@
 
 @section('content')
     @component('components.card', [
+        'dark' => true,
         'size' => 12,
         'sections' => ['Home', 'Spotlights']
     ])
         <h1>Spotlights</h1>
         @if(count($spotlights) > 0)
             @foreach($spotlights as $spotlight)
-                <div class="card bg-light">
+                <div class="card bg-dark">
                     <div class="card-body">
-                        <h3><a href={{route('spotlights.show', ['id' => $spotlight->id])}}>{{$spotlight->title}}</a></h3>
-                        <div class="medium-font float-left">{{$spotlight->description}}</div>
-                        @if($spotlight->deadline != '')
-                            <br />
-                            <div class="medium-font float-left" style="color: #757575;">Deadline: {{substr($spotlight->deadline, 0, -9)}} ({{now()->diffInDays($spotlight->deadline)}} days remaining)</div>
-                        @endif
-                        <div class="medium-font float-right" style="color: #757575;">Created on {{$spotlight->created_at}}</div>
+                        <h3>
+                            <a href="{{ route('spotlights.show', ['id' => $spotlight->id]) }}">
+                                {{ $spotlight->title }}
+                            </a>
+                        </h3>
+
+                        <div class="medium-font">{{ $spotlight->description }}</div>
+
+                        <div class="space-between">
+                            <div class="title-section__info">
+                                Deadline: {{ format_date($spotlight->deadline) }}
+                                ({{ $spotlight->deadline > now() ? '' : '-' . now()->diffInDays($spotlight->deadline) }} days remaining)
+                            </div>
+
+                            <div class="title-section__info">Created on {{ format_date($spotlight->created_at) }}</div>
+                        </div>
                     </div>
-                </div><br />
+                </div>
+
+                <br />
             @endforeach
-        @else
-            <p>no spotlights found...</p>
         @endif
     @endcomponent
 @endsection

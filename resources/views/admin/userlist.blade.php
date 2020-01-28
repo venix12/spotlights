92682b6d1c40eeed480806c503dfd2aa35103101
venix12 <chopek.micha@gmail.com>
2020-01-28 03:04:25 +0100

    refactor manage users

--- resources/views/admin/userlist.blade.php
+++ resources/views/admin/userlist.blade.php
@@ -4,44 +4,53 @@
 
 @section('content')
     @component('components.card', [
+        'dark' => true,
         'sections' => ['Home', 'Manage', 'Manage users']
     ])
-        <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Username</th>
-                <th scope="col">User ID</th>
-                <th scope="col">Usergroup</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($users as $user)
+        @include('components._header', [
+            'title' => 'Manage users',
+        ])
+
+        <table class="table table-dark">
+            <thead class="thead-light">
                 <tr>
-                    <td><a href={{route('user.profile', ['id' => $user->id])}}>{{$user->username}}</a></td>
-                    <td>{{$user->id}}</td>
-                    <td>{{\App\User::GROUPS[$user->group_id]}}</td>
-                    <td>
-                        <div class="row">
-                            @php
-                                $activeValue = $user->active ? 'deactivate' : 'activate';
-                            @endphp
-                            <form action={{route('admin.'.$activeValue.'User')}} method="POST">
-                                @csrf
-                                <input type="hidden" id="userID" name="userID" value="{{$user->id}}">
-                                <input onclick="return confirm('Are you sure you want to {{$activeValue}} {{$user->username}}?')" class="btn btn-dark btn-sm" type="submit" value={{ucfirst($activeValue)}}>
-                            </form> &nbsp;&nbsp;
-                            @if(Auth::user()->isAdmin())
-                                <form action={{route('admin.removeUser')}} method="POST">
-                                    @csrf
-                                    <input type="hidden" id="userID" name="userID" value="{{$user->id}}">
-                                    <input onclick="return confirm('Are you sure you want to remove {{$user->username}}?')" class="btn btn-danger btn-sm" type="submit" value="Remove">
-                                </form>
-                            @endif
-                        </div>
-                    </td>
+                    <th scope="col">Username</th>
+                    <th scope="col">User ID</th>
+                    <th scope="col">Usergroup</th>
+                    <th scope="col">Actions</th>
                 </tr>
-            @endforeach
-        </tbody>
+            </thead>
+            <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>
+                            <a href={{route('user.profile', ['id' => $user->id])}} style="color: {{$user->color}}">
+                                {{$user->username}}
+                            </a>
+                        </td>
+                        <td>{{$user->id}}</td>
+                        <td>{{$user->highestGroup()->identifier ?? ''}}</td>
+                        <td>
+                            <div class="row">
+
+                                @php
+                                    $activeValue = $user->active ? 'deactivate' : 'activate';
+                                @endphp
+
+                                <form action={{route("admin.{$activeValue}User")}} method="POST">
+                                    @csrf
+
+                                    <input type="hidden" id="userID" name="userID" value="{{$user->id}}">
+
+                                    <button type="submit" class="dark-form__button" onclick="return confirm('Are you sure you want to {{$activeValue}} {{$user->username}}?')">
+                                        <i class="fa fa-user"></i> {{ studly_case($activeValue) }}
+                                    </button>
+                                </form>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endcomponent
 @endsection

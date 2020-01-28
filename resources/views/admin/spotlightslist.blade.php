6aa70274532b8bdbf734d7c3a66836bc72f82418
venix12 <chopek.micha@gmail.com>
2020-01-28 03:04:43 +0100

    refactor manage spotlights

--- resources/views/admin/spotlightslist.blade.php
+++ resources/views/admin/spotlightslist.blade.php
@@ -4,52 +4,66 @@
 
 @section('content')
     @component('components.card', [
-        'sections' => ['Home', 'Manage', 'Manage spotlights']
+        'dark' => true,
+        'sections' => ['Home', 'Manage', 'Manage spotlights'],
     ])
-        <table class="table table-striped">
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Creation date</th>
-                <th scope="col">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($spotlights as $spotlight)
+        @include('components._header', [
+            'title' => 'Manage spotlights'
+        ])
+
+        <table class="table table-dark">
+            <thead class="thead-light">
                 <tr>
-                    <td><a href={{route('spotlights.show', ['id' => $spotlight->id])}}>{{$spotlight->title}}</a></td>
-                    <td>{{$spotlight->created_at}}</td>
-                    <td>
-                        <div class="row">
-                            @php
+                    <th scope="col">Name</th>
+                    <th scope="col">Creation date</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($spotlights as $spotlight)
+                    <tr>
+                        <td>
+                            <a href={{route('spotlights.show', ['id' => $spotlight->id])}}>
+                                {{$spotlight->title}}
+                            </a>
+                        </td>
+                        <td>{{$spotlight->created_at}}</td>
+                        <td>
+                            <div class="row">
 
-                                if ($spotlight->active == 1)
-                                {
-                                    $activeValue = "deactivate";
-                                } else {
-                                    $activeValue = "activate";
-                                }
+                                @php
+                                    $activeValue = $spotlight->active ? 'deactivate' : 'activate';
+                                @endphp
 
-                            @endphp
-                            <form action={{route('admin.'.$activeValue.'Spotlights')}} method="POST">
-                            @csrf
-                            <input type="hidden" id="spotlightsID" name="spotlightsID" value="{{$spotlight->id}}">
-                            <input onclick="return confirm('Are you sure you want to {{$activeValue}} {{$spotlight->title}}?')" class="btn btn-dark btn-sm" type="submit" value={{ucfirst($activeValue)}}>
-                            </form>&nbsp;&nbsp;
-                            @if(Auth::user()->isAdmin())
-                                <form action={{route('admin.removeSpotlights')}} method="POST">
+                                <form action={{route("admin.{$activeValue}Spotlights")}} method="POST">
                                     @csrf
+
                                     <input type="hidden" id="spotlightsID" name="spotlightsID" value="{{$spotlight->id}}">
-                                    <input onclick="return confirm('Are you sure you want to remove {{$spotlight->title}}?')" class="btn btn-danger btn-sm" type="submit" value="Remove">
-                                    @if($spotlight->released == 0)
-                                        &nbsp;<button onclick="return confirm('Are you sure you want to mark {{$spotlight->title}} as released?')" type="submit" formaction={{route('spotlights.release')}} class="btn btn-warning btn-sm" type="submit">Release</button>
+
+                                    <button
+                                        type="submit"
+                                        class="dark-form__button"
+                                        onclick="return confirm('Are you sure you want to {{$activeValue}} {{$spotlight->title}}?')"
+                                    >
+                                        <i class="fa fa-lock"></i> {{ studly_case($activeValue) }}
+                                    </button>
+
+                                    @if(Auth::user()->isAdmin() && $spotlight->released !== true)
+                                        <button
+                                            type="submit"
+                                            class="dark-form__button"
+                                            formaction="{{route('spotlights.release')}}"
+                                            onclick="return confirm('Are you sure you want to mark {{$spotlight->title}} as released?')"
+                                        >
+                                            <i class="fa fa-star"></i> Release
+                                        </button>
                                     @endif
                                 </form>
-                            @endif
-                        </div>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     @endcomponent
 @endsection

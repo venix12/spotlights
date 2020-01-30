72598f60608b0e969511deb01fca9dc32f212c70
venix12 <chopek.micha@gmail.com>
2020-01-30 14:11:34 +0100

    make vote columns fillable

--- app/SpotlightsNominationVote.php
+++ app/SpotlightsNominationVote.php
@@ -6,6 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class SpotlightsNominationVote extends Model
 {
+    protected $fillable = [
+        'comment',
+        'comment_updated_at',
+        'nomination_id',
+        'spots_id',
+        'user_id',
+        'value',
+    ];
+
+
     public function scopeCurrentUserSpots($query, $user_id, $spotlights_id)
     {
         return $query->where('user_id', $user_id)->where('spots_id', $spotlights_id);

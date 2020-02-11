8e860d685d3b93ca4bae50b1131e9f49737a056c
Venix <30481900+venix12@users.noreply.github.com>
2020-02-11 11:51:31 +0100

    fractal transform helper

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,26 @@
 <?php
 
-function format_date(string $date, bool $hour = false)
+use App\Serializers\ApiSerializer;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
+use League\Fractal\Manager;
+use League\Fractal\Resource\Collection;
+
+function format_date(string $date, bool $hour = false) : string
 {
     $formatted = substr($date, 0, $hour ? -3 : -9);
 
     return $formatted;
+}
+
+function fractal_transform(EloquentCollection $entries, string $transformer) : array
+{
+    $fractal = new Manager;
+    $fractal->setSerializer(new ApiSerializer);
+
+    $transformer = 'App\\Transformers\\' . $transformer;
+    $transformer = new $transformer;
+
+    $collection = new Collection($entries, $transformer);
+
+    return $fractal->createData($collection)->toArray();
 }

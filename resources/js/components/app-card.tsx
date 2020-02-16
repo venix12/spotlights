b5c4aee6dbb3bce10871c38ec8334780b80ac3e2
Venix <30481900+venix12@users.noreply.github.com>
2020-02-16 14:28:08 +0100

    react app eval

---
+++ resources/js/components/app-card.tsx
@@ -0,0 +1,210 @@
+import React from 'react';
+import Modal from './modal';
+import Axios from 'axios';
+
+interface Props {
+    application: Application,
+}
+
+interface State {
+    approved?: boolean,
+    feedback?: string,
+    feedback_author?: User,
+    modal: boolean,
+    verdict?: 'fail' | 'pass',
+}
+
+class AppCard extends React.Component<Props, State> {
+    state = {
+        approved: this.props.application.approved,
+        feedback: this.props.application.feedback,
+        feedback_author: this.props.application.feedback_author,
+        modal: false,
+        verdict: this.props.application.verdict,
+    }
+
+    approveFeedback = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        console.log(laroute.route(
+            'admin.app-eval.approve-feedback',
+            { id: this.props.application.id }
+        ));
+
+        Axios.post(
+            laroute.route(
+                'admin.app-eval.approve-feedback',
+                { id: this.props.application.id }
+            )
+        );
+
+        this.setState({
+            approved: true,
+        });
+    }
+
+    render() {
+        const { application } = this.props;
+        const { modal } = this.state;
+        return (
+            <div>
+                {modal &&
+                    <Modal
+                        title={`application evaluation - ${application.user.username}`}
+                        content={this.renderModalContent()}
+                        close={this.switchModal}
+                    />
+                }
+
+                <div className="app-card" onClick={this.switchModal}>
+                    <div className="app-card__name">
+                        {application.user.username}
+                    </div>
+
+                    <div className="app-card__info">
+                        {`applied at ${application.created_at}`}
+                    </div>
+
+                    <div className="app-card__info">
+                        {`verdict: ${application.verdict ?? 'not set'}`}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    renderApproveButton() {
+        return (
+            <form onSubmit={this.approveFeedback}>
+                <button className="dark-form__button dark-form__button--top" type="submit">
+                    <i className="fa fa-check" /> Approve feedback!
+                </button>
+            </form>
+        );
+    }
+
+    renderModalContent() {
+        const { application } = this.props;
+        const { approved } = this.state;
+
+        return (
+            <div>
+                {application.answers.map(answer => {
+                    return <>
+                        <h5>{answer.question}</h5>
+                        <div className="dark-section">
+                            {answer.answer}
+                        </div>
+                    </>
+                })}
+
+                <hr />
+
+                {approved ? this.renderVerdictInfoField() : this.renderVerdictField()}
+                {(authUser.is_admin && application.feedback && !approved) && this.renderApproveButton()}
+            </div>
+        )
+    }
+
+    renderStatus() {
+        const { feedback_author, verdict } = this.state;
+
+        return (
+            <span>
+                {verdict &&
+                    <span className="info-badge">
+                        <span className={verdict === 'pass' ? 'text-success' : 'text-danger'}>
+                            {verdict}
+                        </span>
+                    </span>
+                }
+
+                {feedback_author &&
+                    <i className="text-lightgray">
+                        {` written by ${feedback_author.username}`}
+                    </i>
+                }
+            </span>
+        );
+    }
+
+    renderVerdictField() {
+        const { application } = this.props;
+        const { feedback } = this.state;
+        const buttonText = application.verdict ? 'Update' : 'Submit';
+
+        return (
+            <>
+                <form onSubmit={this.storeFeedback}>
+                    <div className="textarea-border">
+                        <textarea id="feedback" placeholder="write feedback here...">{feedback}</textarea>
+                    </div>
+
+                    <div className="form--top space-between">
+                        <span>
+                            {this.renderStatus()}
+                        </span>
+
+                        <div>
+                            <select id="verdict" className="dark-form__select">
+                                <option>fail</option>
+                                <option>pass</option>
+                            </select>
+
+                            <button type="submit" className="dark-form__button dark-form__button--left">
+                                <i className="fa fa-check" /> {buttonText}!
+                            </button>
+                        </div>
+                    </div>
+                </form>
+            </>
+        );
+    }
+
+
+
+    renderVerdictInfoField() {
+        const { feedback } = this.state;
+
+        return (
+            <div>
+                <h5>Feedback</h5>
+                <div className="dark-section">
+                    {feedback}
+                </div>
+
+                {this.renderStatus()}
+            </div>
+        );
+    }
+
+    storeFeedback = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const feedback = (document.getElementById('feedback') as HTMLTextAreaElement).value;
+        const verdict = (document.getElementById('verdict') as HTMLSelectElement).value;
+
+        const res = await Axios.post(
+            laroute.route('admin.app-eval.store-feedback', { id: this.props.application.id }), {
+                feedback: feedback,
+                verdict: verdict,
+            }
+        );
+
+        const data = res.data;
+
+        this.setState({
+            feedback: data.feedback,
+            feedback_author: data.feedback_author,
+            verdict: data.verdict,
+        });
+    }
+
+    switchModal = () => {
+        const { modal } = this.state;
+
+        this.setState({ modal: modal ? false : true});
+    }
+}
+
+export default AppCard;
---
+++ resources/js/components/app-eval.tsx
@@ -0,0 +1,96 @@
+import React from 'react';
+import { render } from '../helpers/render';
+import { parseJson } from '../helpers/helpers';
+import AppCard from './app-card';
+
+interface Props {
+    applications: Application[],
+}
+
+interface State {
+    applications: Application[],
+    currentFilter?: string,
+}
+
+const modes = ['osu!', 'osu!taiko', 'osu!mania', 'osu!catch'];
+
+class AppEval extends React.Component<Props, State> {
+    state = {
+        applications: this.props.applications,
+        currentFilter: '',
+    }
+
+    filterByMode = (mode: string) => {
+        const { applications } = this.props;
+        const { currentFilter } = this.state;
+
+        if (mode === currentFilter) {
+            this.setState({
+                applications: applications,
+                currentFilter: ''
+            });
+
+            return;
+        }
+
+        const appsFiltered = applications.filter(x => x.gamemode === mode);
+
+        this.setState({
+            applications: appsFiltered,
+            currentFilter: mode,
+        });
+    }
+
+    render() {
+        const { applications } = this.state;
+
+        return (
+            <div>
+                {this.renderModeFilter()}
+
+                <h5>Pending applications</h5>
+                {this.renderAppCards(applications.filter(x => x.approved === false))}
+
+                <h5>Finished applications</h5>
+                {this.renderAppCards(applications.filter(x => x.approved === true))}
+            </div>
+        );
+    }
+
+    renderAppCards(apps: Application[]) {
+        return (
+            <div className="dark-section">
+                <div className="app-cards">
+                    {apps.map(app => {
+                        return <AppCard application={app} />
+                    })}
+                </div>
+            </div>
+        );
+    }
+
+    renderModeFilter() {
+        const { currentFilter } = this.state;
+
+        return (
+            <div className="navigation">
+                {modes.map((mode, index) => {
+                    return <span
+                        className={mode === currentFilter
+                            ? 'navigation__el navigation__el--current'
+                            : 'navigation__el'
+                        }
+                        key={index}
+                        onClick={() => this.filterByMode(mode)}
+                    >
+                        {mode}
+                    </span>
+                })}
+            </div>
+        );
+    }
+}
+
+render('app-eval', AppEval, {
+    applications: parseJson('json-apps'),
+})

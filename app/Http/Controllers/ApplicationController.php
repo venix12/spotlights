540ad9271218fd54d3b03c2b4f0474e50ea114bb
Venix <30481900+venix12@users.noreply.github.com>
2020-02-16 14:36:31 +0100

    app form middleware auth

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -9,6 +9,11 @@ use App\AppQuestion;
 
 class ApplicationController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         $questions = AppQuestion::active()->get();

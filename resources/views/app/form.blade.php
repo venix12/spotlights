5089b4871152b0d4d6c302c541e0a110e8f3630c
Venix <30481900+venix12@users.noreply.github.com>
2020-02-16 14:50:21 +0100

    require active cycle to app

--- resources/views/app/form.blade.php
+++ resources/views/app/form.blade.php
@@ -11,58 +11,64 @@
             'title' => 'Application to the osu! Spotlights Team'
         ])
 
-        <form action="{{ route('app.store') }}" method="POST">
-            @csrf
+        @if(App\AppCycle::isActive())
+            <form action="{{ route('app.store') }}" method="POST">
+                @csrf
 
-            @foreach($questions as $question)
+                @foreach($questions as $question)
 
-                @php
-                    $rowCount = round($question->char_limit / 100) + 2;
-                @endphp
+                    @php
+                        $rowCount = round($question->char_limit / 100) + 2;
+                    @endphp
 
-                <div class="card-body bg-dark">
-                    <h4>{{$question->question}} {!! $question->required ? '' : '<span class="text-muted">(optional)</span>' !!}</h4>
+                    <div class="card-body bg-dark">
+                        <h4>{{$question->question}} {!! $question->required ? '' : '<span class="text-muted">(optional)</span>' !!}</h4>
 
-                    <div class="textarea-border">
-                        <textarea class="textarea-invisible"
-                            id="{{$question->id}}"
-                            name="{{$question->id}}"
-                            oninput="countChars(this.id, this.value.length);"
-                            maxlength={{$question->char_limit}}
-                            rows={{$rowCount}}
-                            {!! $question->required ? 'required' : '' !!}
-                        ></textarea>
+                        <div class="textarea-border">
+                            <textarea class="textarea-invisible"
+                                id="{{$question->id}}"
+                                name="{{$question->id}}"
+                                oninput="countChars(this.id, this.value.length);"
+                                maxlength={{$question->char_limit}}
+                                rows={{$rowCount}}
+                                {!! $question->required ? 'required' : '' !!}
+                            ></textarea>
 
-                        <div class="d-flex justify-content-end">
-                            <div class="title-section__info">
-                                <span id="{{$question->id}}-counter">0</span>
-                                <span>&nbsp;/ {{$question->char_limit}}</span>
+                            <div class="d-flex justify-content-end">
+                                <div class="title-section__info">
+                                    <span id="{{$question->id}}-counter">0</span>
+                                    <span>&nbsp;/ {{$question->char_limit}}</span>
+                                </div>
                             </div>
                         </div>
                     </div>
+
+                    {!! $loop->last ? '' : '<br>' !!}
+                @endforeach
+                <hr>
+
+                <div class="text-center">
+                    @if(Auth::check() && !Auth::user()->isMember())
+                        gamemode <select name="gamemode" class="dark-form__select">
+                            <option value="osu">osu!</option>
+                            <option value="taiko">osu!taiko</option>
+                            <option value="mania">osu!mania</option>
+                            <option value="catch">osu!catch</option>
+                        </select> <br>
+
+                        <button type="submit" class="dark-form__button dark-form__button--top-rem flex-centre">
+                            <i class="fa fa-user"></i> Apply now!
+                        </button>
+                    @else
+                        <span class="text-muted">you are already a member!</span>
+                    @endif <br>
                 </div>
-
-                {!! $loop->last ? '' : '<br>' !!}
-            @endforeach
-            <hr>
-
-            <div class="text-center">
-                @if(Auth::check() && !Auth::user()->isMember())
-                    gamemode <select name="gamemode" class="dark-form__select">
-                        <option value="osu">osu!</option>
-                        <option value="taiko">osu!taiko</option>
-                        <option value="mania">osu!mania</option>
-                        <option value="catch">osu!catch</option>
-                    </select> <br>
-
-                    <button type="submit" class="dark-form__button dark-form__button--top-rem flex-centre">
-                        <i class="fa fa-user"></i> Apply now!
-                    </button>
-                @else
-                    <span class="text-muted">you are already a member!</span>
-                @endif <br>
+            </form>
+        @else
+            <div class="d-flex justify-content-center text-lightgray">
+                there's no active application cycle at the moment!
             </div>
-        </form>
+        @endif
     @endcomponent
 @endsection
 

6073f3113640e9c5d4cf6736b028a92de63b66c2
Venix <30481900+venix12@users.noreply.github.com>
2020-02-16 14:29:16 +0100

    require app eval component

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,11 @@
 
 require('./bootstrap');
 
+
 /**
  * Next, we will create a fresh React component instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
+
+require('./components/app-eval');

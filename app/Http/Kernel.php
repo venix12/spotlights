20fdf50d5f4969aa887ed5670e85e19fcb69ad30
Venix <30481900+venix12@users.noreply.github.com>
2020-02-25 14:13:38 +0100

    register member middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -59,6 +59,7 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'is_admin' => \App\Http\Middleware\IsAdmin::class,
         'is_admin_or_manager' => \App\Http\Middleware\IsAdminOrManager::class,
+        'is_member' => \App\Http\Middleware\IsMember::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,

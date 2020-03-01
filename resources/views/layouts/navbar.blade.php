11080c81f63d5c0600587ceb96c6bacd1fde9a1d
Venix <30481900+venix12@users.noreply.github.com>
2020-03-01 17:14:27 +0100

    oops

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -53,19 +53,6 @@
                         </div>
                     </li>
                 @endauth
-
-                <li class="nav-item">
-
-                    <a class="dropdown-item" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                        {{ __('Logout') }}
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        @csrf
-                    </form>
-                </li>
             </ul>
         </div>
     </div>

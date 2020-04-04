7999027f4d16ac4de93262dd6a37782f4254ce61
Venix <30481900+venix12@users.noreply.github.com>
2020-04-04 18:49:10 +0200

    refactor card code

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,32 +1,15 @@
 @php
-    if(!isset($dark))
-    {
-        $dark = false;
-    }
-
     if(!isset($size))
     {
         $size = 8;
     }
-
-    $dark ? $bodyClass = 'bg-gray text-white' : $bodyClass = '';
-    $dark ? $borderStyle = 'border: none' : $borderStyle = '';
-    $dark ? $headerClass = 'bg-dark text-white' : $headerClass = '';
 @endphp
 
-@if($dark === true)
-    <style>
-    a, a:hover, a:focus {
-        color: white;
-    }
-    </style>
-@endif
-
-<div class="container">
+<div class="container container-main">
     <div class="row justify-content-center">
-        <div class="col-md-{{$size}}">
-            <div class="card" style="{{$borderStyle}}">
-                <div class="card-header {{$headerClass}}">
+        <div class="col-md-{{$size}} col-main">
+            <div class="card-layout">
+                <div class="card-layout__header">
                     <div class="sections-header">
                         @foreach($sections as $section)
                             <span class="sections-header__el">
@@ -35,7 +18,7 @@
                         @endforeach
                     </div>
                 </div>
-                <div class="card-body {{$bodyClass}}">
+                <div class="card-layout__content">
                     @include('layouts.session')
 
                     {{$slot}}

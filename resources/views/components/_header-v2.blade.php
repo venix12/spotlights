bf934d415e13e89aa30b807c9b38825a80c88983
Venix <30481900+venix12@users.noreply.github.com>
2020-06-27 13:16:23 +0200

    new header design

--- resources/views/components/_header-v2.blade.php
+++ resources/views/components/_header-v2.blade.php
@@ -1,12 +1,20 @@
 <div class="dark-section dark-section--header-section">
-    <div class="header-v2 {{ isset($colour) ? 'header-v2--' . $colour : '' }}">
-        <div class="header-v2__title">
-            <i class="fa fa-{{ $icon }}"></i> {{ $title }}
+    <div class="header-v2">
+        <div class="header-v2__icon-container">
+            <div class="header-v2__icon">
+                <i class="fa fa-{{ $icon }}"></i>
+            </div>
         </div>
 
-        @if (isset($description))
-            <div class="header-v2__description">{{ $description }}</div>
-        @endif
+        <div class="header-v2__title-container">
+            <div class="header-v2__title">
+                {{ $title }}
+            </div>
+
+            @if (isset($description))
+                <div class="header-v2__description">{{ $description }}</div>
+            @endif
+        </div>
     </div>
 </div>
 <div class="dark-section-divider"></div>

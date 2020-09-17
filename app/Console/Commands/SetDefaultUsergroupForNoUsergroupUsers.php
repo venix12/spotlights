5fdacddf27b01dc1bf0dbbc0f23a24d6a30e467d
Venix <30481900+venix12@users.noreply.github.com>
2020-09-17 14:41:38 +0200

    add command to add all users with no group to default

---
+++ app/Console/Commands/SetDefaultUsergroupForNoUsergroupUsers.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Group;
+use App\User;
+use App\UserGroup;
+use Illuminate\Console\Command;
+
+class SetDefaultUsergroupForNoUsergroupUsers extends Command
+{
+    protected $signature = 'usergroup:default';
+
+    protected $description = 'Sets usergroup to default for all users that don\'t belong to any usergroup';
+
+    public function handle()
+    {
+        $users = User::all()->filter(function ($user) {
+            return $user->highestGroup() === null;
+        });
+
+        $bar = $this->output->createProgressBar(count($users));
+        $bar->start();
+
+        foreach ($users as $user) {
+            UserGroup::create([
+                'group_id' => Group::byIdentifier('default')->id,
+                'user_id' => $user->id,
+            ]);
+
+            $bar->advance();
+        }
+
+        $bar->finish();
+        $this->output->newLine();
+    }
+}
